b566a1898fded628ad06de9402405b8a1090f73e
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2023-01-12 17:55:37 -0600

    Revert "Merge branch 'Work-in-Progress' of https://github.com/4198-Programmers/Charged-Up-2023 into Work-in-Progress"
    This reverts commit 57687daddbbf18ea428c794cf7c480007c09cbaa, reversing
    changes made to 2fc2bf8825aa81637ebb77fbd93208b1e35aaf3e.

--- 2023 Code-Updated/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2023 Code-Updated/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,9 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.wpiVcontainer.SwerveModule;
 
 public class DriveTrain extends SubsystemBase {
-  SwerveModule frontL = new SwerveModule(Constants.FRONT_LEFT_DRIVE_MOTOR,Constants.FRONT_LEFT_SPIN_MOTOR,Constants.FRONT_LEFT_CANCODER,Constants.FRONT_LEFT_DRIVE_ENCODER,Constants.FRONT_LEFT_CANCODER_OFFSET);
+  SwerveModule frontL = new SwerveModule(1,3,4,2,8);
 }
