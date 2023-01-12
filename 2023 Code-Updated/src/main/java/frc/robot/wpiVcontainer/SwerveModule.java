4fe167674dfef5c338cb71480037786a46dd19fc
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2023-01-12 16:07:08 -0600

    Some comments

--- 2023 Code-Updated/src/main/java/frc/robot/wpiVcontainer/SwerveModule.java
+++ 2023 Code-Updated/src/main/java/frc/robot/wpiVcontainer/SwerveModule.java
@@ -6,21 +6,16 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
-    private CANSparkMax driveMotor;
-    private CANSparkMax spinMotor;
-    private CANCoder spinEncoder;
-    private RelativeEncoder driveEncoder;
-    private double spinOffset;
-    private double spinPos;
-    private Rotation2d rotation;
+    private CANSparkMax driveMotor; // motor that drives the wheel
+    private CANSparkMax spinMotor;// motor that spins the wheel around to face different ways
+    private CANCoder spinEncoder; // cancoder, just named enco
+    private RelativeEncoder driveEncoder; // encoder for drvie motor
+    private double spinOffset; // distance from our zero to the magnetic 0 in the cancoders
+    private double spinPos; // output of the cancoder, from 0-360
+    private Rotation2d rotation; // turns cancoder value to valid rotation value for later use
 
     public SwerveModule(int driveID, int spinID, int CANCoderID, int driveEncoderID, double CANCoderZeroOffset) {
         driveMotor = new CANSparkMax(driveID, MotorType.kBrushless);
@@ -29,13 +24,18 @@ public class SwerveModule {
         driveEncoder = driveMotor.getEncoder();
         spinOffset = CANCoderZeroOffset;
 
-        spinPos = spinEncoder.getAbsolutePosition()-spinOffset;
+        if (spinPos < 0) {
+            spinPos = 360 + spinEncoder.getAbsolutePosition() - spinOffset; // makes sure that the number stays between
+                                                                            // 0-360 instead of going negative
+        } else {
+            spinPos = spinEncoder.getAbsolutePosition() - spinOffset;
+        }
         rotation = Rotation2d.fromDegrees(spinPos);
 
     }
 
-    public void setDesiredState(SwerveModuleState desiredState){
-        //allows for optimization
+    public void setDesiredState(SwerveModuleState desiredState) {
+        // allows for optimization
         SwerveModuleState state = SwerveModuleState.optimize(desiredState, rotation);
     }
 
