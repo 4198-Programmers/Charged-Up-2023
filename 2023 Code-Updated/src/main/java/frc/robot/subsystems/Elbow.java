5f4b59969ae97abaeac8f12723f332f1884485d7
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2023-01-17 16:35:14 -0600

    update

--- 2023 Code-Updated/src/main/java/frc/robot/subsystems/Elbow.java
+++ 2023 Code-Updated/src/main/java/frc/robot/subsystems/Elbow.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,8 +9,22 @@ import frc.robot.Constants;
 
 public class Elbow extends SubsystemBase{
     CANSparkMax elbowMotor = new CANSparkMax(Constants.ELBOW_MOTOR, MotorType.kBrushless);
+    RelativeEncoder elbowEncoder = elbowMotor.getEncoder();
 
     public void setSpeed(double speed){
         elbowMotor.set(speed);
+    }
+    public double getPosition(){
+        return elbowEncoder.getPosition();
+    }
+    public void setSpeedWithLimits(double speed){
+        double expectedSpeed = speed;
+        if(speed > 0 && getPosition() > Constants.ELBOW_UPPER_LIMIT){
+            expectedSpeed = 0;
+        }
+        else if(speed < 0 && getPosition() < Constants.ELBOW_LOWER_LIMIT){
+            expectedSpeed = 0;
+        }
+        elbowMotor.set(expectedSpeed);
     }
 }
--- 2023 Code-Updated/src/main/java/frc/robot/subsystems/Hand.java
+++ 2023 Code-Updated/src/main/java/frc/robot/subsystems/Hand.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,8 +9,22 @@ import frc.robot.Constants;
 
 public class Hand extends SubsystemBase{
     CANSparkMax handMotor = new CANSparkMax(Constants.HAND_MOTOR, MotorType.kBrushless);
+    RelativeEncoder handEncoder = handMotor.getEncoder();
 
     public void setSpeed(double speed){
         handMotor.set(speed);
+    }
+    public double getLocation(){
+        return handEncoder.getPosition();
+    }
+    public void setSpeedWithLimits(double speed){
+        double expectedSpeed = speed;
+        if(speed > 0 && getLocation() >= Constants.HAND_UPPER_LIMIT){
+            expectedSpeed = 0;
+        }
+        else if(speed < 0 && getLocation() <= Constants.HAND_LOWER_LIMIT){
+            expectedSpeed = 0;
+        }
+        handMotor.set(expectedSpeed);
     }
 }
