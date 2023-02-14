fc97f962d7fc2522381223d773a287705d0dfbba
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2023-02-14 17:49:42 -0600

    Finished removing apriltags code

--- Re-do_2023/src/main/java/frc/robot/Maths.java
+++ Re-do_2023/src/main/java/frc/robot/Maths.java
@@ -1,8 +1,5 @@
 package frc.robot;
 
 public class Maths {
-    public static double DistanceFromTarget(double pitch) {
-        double distanceToTarget = Constants.CAMERA_TO_APRILTAG_HEIGHT_DIFFERENCE/Math.tan(Math.toRadians(pitch));
-        return distanceToTarget;
-    }
+
 }
