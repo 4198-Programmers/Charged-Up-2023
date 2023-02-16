e92f1173dbca631b751d72938bd12009ece95c35
whiteA <60160962+41robocats98@users.noreply.github.com>
2023-02-16 17:46:10 -0600

    Revert "Removed AprilTags code"
    This reverts commit 3a4f4269d768f497609c1dd84ccad1bac368ac0e.

--- Re-do_2023/src/main/java/frc/robot/Maths.java
+++ Re-do_2023/src/main/java/frc/robot/Maths.java
@@ -8,4 +8,8 @@ public class Maths {
         }
         return angle;
     }
+    public static double DistanceFromTarget(double pitch) {
+        double distanceToTarget = Constants.CAMERA_TO_APRILTAG_HEIGHT_DIFFERENCE/Math.tan(Math.toRadians(pitch));
+        return distanceToTarget;
+    }
 }
