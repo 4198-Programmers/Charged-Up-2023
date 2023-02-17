a9887ff05b20d079b8791e9cfb8738a4720c7f72
whiteA <60160962+41robocats98@users.noreply.github.com>
2023-02-17 15:04:44 -0600

    Vision?

---
+++ Re-do_2023/src/main/java/frc/robot/Commands/CheckPhotonTarget.java
@@ -0,0 +1,21 @@
+// package frc.robot.Commands;
+
+// import org.photonvision.PhotonCamera;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.Subsystems.PhotonVision;
+
+// public class CheckPhotonTarget extends CommandBase{
+//     private PhotonVision vision; 
+//     public CheckPhotonTarget(PhotonVision photonVision) {
+//         super();
+//         this.vision = photonVision;
+//         addRequirements(photonVision);
+//     }
+
+//     @Override
+//     public void execute() {
+//         this.vision.getBestTarget();
+//     }
+
+
+// }
