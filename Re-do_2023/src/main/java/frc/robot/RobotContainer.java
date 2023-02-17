27bf7a155264fadc3212ca83cce2afe47ee72606
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2023-02-16 18:55:49 -0600

    Update RobotContainer.java

--- Re-do_2023/src/main/java/frc/robot/RobotContainer.java
+++ Re-do_2023/src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,7 @@ package frc.robot;
 import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
