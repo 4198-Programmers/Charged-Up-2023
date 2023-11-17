002190ea4125a00e352533251a1696c6d748e89b
EmilyKram <90582499+EmilyKram@users.noreply.github.com>
2023-11-17 12:17:42 -0600

    Updated SetAngle Comments

--- Re-do_2023/src/main/java/frc/robot/AttemptFour/SwerveModule.java
+++ Re-do_2023/src/main/java/frc/robot/AttemptFour/SwerveModule.java
@@ -122,56 +122,38 @@ public class SwerveModule {
             if(wantedAngle > 0){
                 if(currentAngle > 0){
                     if(wantedAngle > currentAngle){
-                        /*  angleDiff < 180
-                            wantedAngle > 0
-                            currentAngle > 0
-                            wantedAngle > currentAngle */
+                        /*  angleDiff < 180; wantedAngle > 0; currentAngle > 0; wantedAngle > currentAngle */
                         speed *= 1;
                     }else{
-                        /*  angleDiff < 180
-                            wantedAngle > 0
-                            currentAngle > 0
-                            wantedAngle < currentAngle */
+                        /*  angleDiff < 180; wantedAngle > 0; currentAngle > 0; wantedAngle < currentAngle */
                         speed *= -1;
                     }
                 }else{
-                    /*  angleDiff < 180
-                        wantedAngle > 0
-                        currentAngle < 0 */
+                    /*  angleDiff < 180; wantedAngle > 0; currentAngle < 0 */
                     speed *= 1;
                 }
             }else{
                 if(currentAngle > 0){
-                    /*  angleDiff < 180
-                        wantedAngle < 0
-                        currentAngle > 0 */
+                    /*  angleDiff < 180; wantedAngle < 0; currentAngle > 0 */
                     speed *= -1;
                 }
             }
         }else{
             if(wantedAngle > 0){
                 if(currentAngle < 0){
-                    /*  angleDiff > 180
-                        wantedAngle > 0
-                        currentAngle < 0 */
+                    /*  angleDiff > 180; wantedAngle > 0; currentAngle < 0 */
                     speed *= -1;
                 }
             }else{
                 if(currentAngle > 0){
-                    /*  angleDiff > 180
-                        wantedAngle < 0
-                        currentAngle > 0 */
+                    /*  angleDiff > 180; wantedAngle < 0; currentAngle > 0 */
                     speed *= 1;
                 }else{
                     if(wantedAngle < currentAngle){
-                        /*  wantedAngle < 0
-                            currentAngle < 0
-                            wantedAngle < currentAngle */
+                        /*  wantedAngle < 0; currentAngle < 0; wantedAngle < currentAngle */
                         speed *= -1;
                     }else{
-                        /*  wantedAngle < 0
-                            currentAngle < 0
-                            currentAngle < wantedAngle */
+                        /*  wantedAngle < 0; currentAngle < 0; currentAngle < wantedAngle */
                         speed *= 1;
                     }
                 }
@@ -180,41 +162,4 @@ public class SwerveModule {
         speed = Math.abs(speed) > Constants.ANGLE_SPEED_DEADBAND ? speed : 0;
         return speed;
     }
-
-    public static SwerveModuleState optimize(SwerveModuleState desiredState, Rotation2d currentAngle) {
-    double targetAngle = placeInAppropriate0To360Scope(currentAngle.getDegrees(), desiredState.angle.getDegrees());
-    double targetSpeed = desiredState.speedMetersPerSecond;
-    double delta = targetAngle - currentAngle.getDegrees();
-    if (Math.abs(delta) > 90){
-        targetSpeed = -targetSpeed;
-        targetAngle = delta > 90 ? (targetAngle -= 180) : (targetAngle += 180);
-    }        
-    return new SwerveModuleState(targetSpeed, Rotation2d.fromDegrees(targetAngle));
-  }
-
-     private static double placeInAppropriate0To360Scope(double scopeReference, double newAngle) {
-      double lowerBound;
-      double upperBound;
-      double lowerOffset = scopeReference % 360;
-      if (lowerOffset >= 0) {
-          lowerBound = scopeReference - lowerOffset;
-          upperBound = scopeReference + (360 - lowerOffset);
-      } else {
-          upperBound = scopeReference - lowerOffset;
-          lowerBound = scopeReference - (360 + lowerOffset);
-      }
-      while (newAngle < lowerBound) {
-
-          newAngle += 360;
-      }
-      while (newAngle > upperBound) {
-          newAngle -= 360;
-      }
-      if (newAngle - scopeReference > 180) {
-          newAngle -= 360;
-      } else if (newAngle - scopeReference < -180) {
-          newAngle += 360;
-      }
-      return newAngle;
-  }
 }
